94d7c2998b24afd5c98485a8507394ead799d466
梦月時謌 <3465344901@qq.com>
2024-03-25 20:05:43 +0800

    加载界面双击可进入脚本调试界面\nOne of the money

--- hotrun.js
+++ hotrun.js
@@ -7,7 +7,7 @@ if (device.sdkInt >= 23) {
 ui.layout(
     <frame>
         <vertical w="*" h="*" layout_gravity="center">
-            <vertical h="*" gravity="center" >
+            <vertical id="script" h="*" gravity="center"  >
                 <img src="file://./res/icon.png" w="120" h="120" scaleType="fitXY" />
                 <horizontal gravity="center" margin="25">
                     <text id="loadingtext">加载中，请稍后</text>
@@ -16,24 +16,40 @@ ui.layout(
         </vertical>
     </frame>
 );
+var isCanFinish = false;
 
+
+ui.script.click((view) => {
+    if (!isCanFinish) {
+        isCanFinish = true;
+        setTimeout(() => {
+            toastLog("双击进入脚本调试界面");
+            isCanFinish = false;
+        }, 400);
+    } else {
+        engines.execScriptFile("./activity/debug.js", {
+            path: files.path('./activity/'),
+        });
+    };
+
+})
 let cnt = 0;
-setInterval(function () {
+setInterval(function() {
     cnt = (cnt + 1) % 5;
     ui.loadingtext.text('加载中，请稍后' + (new Array(cnt + 1)).join('.'));
 }, 400);
 let _proj_def_n = 'arkplan';
 let path = context.getExternalFilesDir(null).getAbsolutePath() + '/';
 console.setGlobalLogConfig({
-    "file": path+"/"+_proj_def_n+"_log.txt"
+    "file": path + _proj_def_n + "_log.txt"
 });
 
-threads.start(function () {
+threads.start(function() {
     if (device.sdkInt < 24) {
         engines.execScriptFile("./activity/device_usage.js");
-        setTimeout(function () {
+        setTimeout(function() {
             ui.finish();
-        }, 1000);
+        }, 1500);
         return
     }
     let packageName = context.packageName;
@@ -43,13 +59,15 @@ threads.start(function () {
         engines.execScriptFile(files.cwd() + '/../dist/auto.js', {
             path: files.cwd() + '/../'
         });
-        setTimeout(function () {
+        setTimeout(function() {
             ui.finish();
-        }, 1000);
+        }, 1500);
     } else {
 
         if (!files.exists(path + _proj_def_n + '/')) {
-            let { filesx } = require('./modules/ext-files');
+            let {
+                filesx
+            } = require('./modules/ext-files');
 
             filesx.copy(files.path('./'), path);
             files.rename(path + 'project/', _proj_def_n);
@@ -58,9 +76,8 @@ threads.start(function () {
         engines.execScriptFile(path + _proj_def_n + '/main.js', {
             path: path + _proj_def_n + '/'
         });
-        setTimeout(function () {
-            ui.finish();
-        }, 1000);
+        setTimeout(function() {
+                ui.finish();
+        }, 1500);
     }
 });
-
